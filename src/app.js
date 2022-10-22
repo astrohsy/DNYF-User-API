f6f18d548547922cd132121486a52ffb305f3900
Rey Christopher Aparece <ApareceR12@gmail.com>
2022-10-22 14:00:12 -0500

    Adding auth0 boiler plate

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express();
-const routes = require('./routers/users');
+const routers = require('./routers/users');
 require('dotenv').config();
 
 const { auth } = require('express-openid-connect');
 app.use(
     auth({
+        authRequired: false,
+        auth0Logout: true,
         issuerBaseURL: process.env.ISSUER_BASE_URL,
         baseURL: process.env.BASE_URL,
         clientID: process.env.CLIENT_ID,
         secret: process.env.SECRET,
     })
 );
+app.use("/", routers);
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
