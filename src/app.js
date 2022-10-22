0b8910a9e081ba48f257996eeefce76453e4fc88
Rey Christopher Aparece <ApareceR12@gmail.com>
2022-10-22 01:21:31 -0500

    Working on user authentication

--- src/app.js
+++ src/app.js
@@ -1,4 +1,19 @@
 const express = require('express');
-const routes = require('./routers/users')
+const app = express();
+const routes = require('./routers/users');
+require('dotenv').config();
 
-module.exports = router;
+const { auth } = require('express-openid-connect');
+app.use(
+    auth({
+        issuerBaseURL: process.env.ISSUER_BASE_URL,
+        baseURL: process.env.BASE_URL,
+        clientID: process.env.CLIENT_ID,
+        secret: process.env.SECRET,
+    })
+);
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`istening on ${port}`);
+});
