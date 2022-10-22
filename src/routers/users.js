295b082d2ad8bb459c630f824f27e856219fbcbc
Rey Christopher Aparece <ApareceR12@gmail.com>
2022-10-21 22:28:05 -0500

    I am highly caffeinated

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const user_controller = require("../controller/users")
 
+router.get("/users:uid", user_controller.get_user_info)
 router.get("/users")
-
+router.post("/users")
+router.delete("/users:uid")
+router.put("/users:uid")
 module.exports = router;
