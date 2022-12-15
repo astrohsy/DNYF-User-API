67e8ff6402bb76db3cfbda3a8cb16fd51a4d7539
Rey Christopher Aparece <ApareceR12@gmail.com>
2022-12-14 19:08:39 -0600

    Changing | to ||

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -3,11 +3,11 @@ const Sequilize = require("sequelize");
 
 module.exports = new Sequilize({
     dialect: "mysql",
-    host: process.env.DB_HOST | "db",
-    username: process.env.DB_USER | "dbuser",
-    password: process.env.DB_PASS | "dbuser",
+    host: process.env.DB_HOST || "db",
+    username: process.env.DB_USER || "dbuser",
+    password: process.env.DB_PASS || "dbuser",
     port: process.env.DB_PORT || 3306,
-    database: process.env.DB_NAME | "dnyf_users_db",
+    database: process.env.DB_NAME || "dnyf_users_db",
     define: {
         timestamps: false
     },
